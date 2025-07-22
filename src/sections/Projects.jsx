c3cd17dda4ad2b893345b8b79689ce0572a44446
YashPanchal0503 <yash2007panchal@gmail.com>
2025-07-22 19:52:36 +0530

    v1 for deployment

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,5 +1,5 @@
 import { myProjects } from "../constants"
-import Project from "../components/Project";
+import Project from "../components/ProjectSection";
 import { motion, useMotionValue ,useSpring} from 'framer-motion';
 import { useState } from "react";
 
@@ -17,7 +17,7 @@ const Projects = () => {
     <section onMouseMove={handelMouseMove} className="relative c-space section-spacing">
         <h2 className="text-heading"> My Selected Projects</h2>
         <div className="bg-gradient-to-r from-transparent via-neutral-700 to-transparent mt-12 h-[1px] w-full"/>
-        {myProjects.map((project)=>(<Project key={project.id} {...project} setPreview={setPreview}/>))} 
+        {myProjects.map((project)=>(<ProjectSection key={project.id} {...project} setPreview={setPreview}/>))} 
         {preview && (<motion.img 
             className="fixed top-0 left-0 z-50 object-cover h-56 rounded-lg shadow-lg pointer-events-none w-80 " 
             src={preview}
